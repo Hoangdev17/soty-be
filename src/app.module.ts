fd7429046f6bc4345366ec9cd3dae8f4111f9d0e
KienLDG <giakien0401@gmail.com>
2025-09-05 23:17:53 +0700

    feat(structure): update schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './core/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { GuildsModule } from './packages/guilds/guilds.module';
 
 @Module({
-  
-  imports:[
-    ConfigModule.forRoot(), 
+  imports: [
+    ConfigModule.forRoot(),
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    UsersModule, 
-    AuthModule],
-  
+    UsersModule,
+    AuthModule,
+    GuildsModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
