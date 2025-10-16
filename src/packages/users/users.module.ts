2c4a632b899639b15836873e496c3a001aef6f37
Hoangdev17 <hoangluudev17@gmail.com>
2025-10-16 10:14:09 +0700

    feat(friend): friend system

--- src/packages/users/users.module.ts
+++ src/packages/users/users.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { SnowflakeID } from 'src/utils/snowflake';
 import { PrismaService } from 'src/core/prisma/prisma.service';
+import { WebsocketGateway } from '../websocket/websocket.gateway';
+import { WebsocketModule } from '../websocket/websocket.module';
 
 @Module({
-  imports: [],
+  imports: [forwardRef(() => WebsocketModule)],
   controllers: [UsersController],
   providers: [UsersService, SnowflakeID, PrismaService],
   exports: [UsersService],
